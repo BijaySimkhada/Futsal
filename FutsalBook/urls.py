a3eeb441c8045dbe01cb09d48cdb6e883f99461e
Bijay <bijay.simkhada26@gmail.com>
2020-04-10 16:58:19 +0545

    Futsal register

--- FutsalBook/urls.py
+++ FutsalBook/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
+admin.site.site_header = "Futsal Book"
+admin.site.site_title = "Futsal Book"
+admin.site.index_title = "|Admin|Futsal Book"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('futsal.urls')),
     path('account/', include('account.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
